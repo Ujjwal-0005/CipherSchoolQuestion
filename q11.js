a136ddde415491262968c0a61b7af878163a1ce6
Ujjwal-0005 <151321517+Ujjwal-0005@users.noreply.github.com>
2025-07-08 15:44:51 +0530

    eleventh question

---
+++ q11.js
@@ -0,0 +1,7 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+const person1 = new Person("Alice", 30);
+console.log("Name:", person1.name); 
+console.log("Age:", person1.age);   // Output- 30
